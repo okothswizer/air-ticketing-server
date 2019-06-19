bd0eacc7d57e7693cc4233cdc8f60b28a914b8d0
okoth swizer <okoth.samson89@gmail.com>
2019-06-19 16:29:26 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const express=require("express");
+const express = require("express");
 
 const mysql = require("mysql");
 
-const app=express();
-const pool = mysql .createPool({
+const app = express();
+const pool = mysql.createPool({
     host: "localhost",
     user: "foo",
-    password:"password",
-    database:"air-ticketing"
+    password: "password",
+    database: "air-ticketing"
 });
 app.get("/api/flight", (req, res) => {
     pool.query("SELECT id, arrival time FROM flight", (error, rows) => {
-        if (Error) {
-            return res.start(500).Json({error});
-            {
-                res . Json(rows);
+        if (error) {
+            return res.status(500).json({ error });
+        }
+        res.json(rows);
+    });
+});
 
-        app.listen(3306, () => console.log("App listening on port 3306"));
-
-
-app.listen(9000, () => console.log("App listening on port 9000"));
+app.listen(3306, () => {
+    console.log("App running on port 3306");
+});
