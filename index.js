2ca274bbbd1ca28d10c3adb6cd9fdf2b8f1720b1
okoth swizer <okoth.samson89@gmail.com>
2019-06-19 16:44:41 +0300

    add nodemon globally

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ app.get("/api/flight", (req, res) => {
     });
 });
 
-app.listen(3306, () => {
-    console.log("App running on port 3306");
+app.listen(9000, () => {
+    console.log("App running on port 9000");
 });
