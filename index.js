3b53200b34f41724081b7bd997976ad96dc78744
okoth swizer <okoth.samson89@gmail.com>
2019-07-15 19:21:25 +0300

    api post

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ const pool = mysql.createPool({
     database: "air_ticketing"
 
 });
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
 app.get("/api/customer", (req, res) => {
-    pool.query("SELECT id, name FROM customer", (error, rows) => {
+    pool.query("SELECT name FROM customer", (error, rows) => {
         if (error) {
             return res.status(500).json({ error });
         }
@@ -24,7 +25,7 @@ app.get("/api/customer", (req, res) => {
 });
 app.get("/api/customer/:id", (req, res) => {
          pool.query(
-             "SELECT id, name FROM customer WHERE id = ?",
+             "SELECT name FROM customer WHERE id = ?",
 
             [req.params.id],
             (error, rows) => {
@@ -39,7 +40,7 @@ app.get("/api/customer/:id", (req, res) => {
     
     app.get("/api/flight/:id", (req, res) => {
              pool.query(
-            " SELECT id, flight_number,departure, destination FROM flight WHERE id = ?",
+            " SELECT flight_number,departure, destination FROM flight WHERE id = ?",
     
                 [req.params.id],
                 (error, rows) => {
@@ -78,15 +79,15 @@ app.get("/api/customer/:id", (req, res) => {
         })
 })    
 
-app.post('/api/customer', (req, res) => {
-    const customer= req.body;
-    if (!customer.name,id, mobile_number) {
+app.post('/api/customer/:id', (req, res) => {
+    const { name, mobile_number} = req.body;
+    if ( !name|| !mobile_number) {
         return res.status(400).json({ error: "invalid payload"});
     }
 
     pool.query(
-        "INSERT INTO customer (id, name, mobile_number) VALUES (5, 'sam', 723678542)",
-        [id, name, mobile_number],
+        "INSERT INTO customer ( name, mobile_number) VALUES (?,?)",
+        [ name, mobile_number,req.body.params],
         (error, results) => {
             if (error) {
                 return res.status(500).json({error});
@@ -95,4 +96,42 @@ app.post('/api/customer', (req, res) => {
         }
     )
 })
+app.post("/api/customer/:Id", (req, res) => {
+    const customer = req.body;
+    if (!customer.name) {
+        return res.status(400).json({ error: "invalid payload"});
+    }
+
+    pool.query(
+        "INSERT INTO customer ( name, mobile_number) VALUES (?,?)",
+        [customer.name],
+        (error, results) => {
+            if (error) {
+                return res.status(500).json({error});
+            }
+            res.json(results.insertId);
+        }
+    )
+})
+
+app.post("/api/flight_ticket_number/:Id", (req, res) => {
+    const flight_ticket_number = req.body;
+    if (!flight_ticket_number.name) {
+        return res.status(400).json({ error: "invalid payload"});
+    }
+
+    pool.query(
+        "INSERT INTO flight_ticket_number ( ticket_number) VALUE (?)",
+        [flight_ticket_number.name],
+        (error, results) => {
+            if (error) {
+                return res.status(500).json({error});
+            }
+            res.json(results.insertId);
+        }
+    )
+})
+
+
+
 app.listen(9000, () => console.log("App listening on port 9000"));
